1a887eb98c301b54ea80ffab0921079486523a12
Zeev-ops <116187472+Zeev-ops@users.noreply.github.com>
2024-08-16 22:29:07 +0000

    turned most paths private

--- utils/geo_utils.py
+++ utils/geo_utils.py
@@ -1,48 +1,42 @@
-
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import os
-import seaborn as sns
 from collections import defaultdict
 
 
 def read_prefixes_from_metadata_txt(txt_path, prefix= '!Sample_', separator='\t'):
+  # Reading...
+  with open(txt_path, 'r') as file:
+    lines = file.readlines()
 
-    # Reading...
-    with open(txt_path, 'r') as file:
-        lines = file.readlines()
+  # I only want lines starting with !Sample_ 
+  sample_lines = [line.strip() for line in lines if line.startswith(prefix)]
 
-    # I only want lines starting with !Sample_ 
-    sample_lines = [line.strip() for line in lines if line.startswith(prefix)]
-
-    # Parse the data
-    headers = []
-    data_dict = defaultdict(list)
+  # Parse the data
+  headers = []
+  data_dict = defaultdict(list)
+  
+  for line in sample_lines:
+    parts = line.split(separator)
+    header = parts[0]
+    data = parts[1:]
     
-    for line in sample_lines:
-        parts = line.split(separator)
-        header = parts[0]
-        data = parts[1:]
-        
-        # If header already exists, append a suffix to make it unique
-        count = 1
-        new_header = header
-        while new_header in headers:
-            count += 1
-            new_header = f"{header}.{count}"
-        
-        headers.append(new_header)
-        data_dict[new_header] = data
+    # If header already exists, append a suffix to make it unique
+    count = 1
+    new_header = header
+    while new_header in headers:
+      count += 1
+      new_header = f"{header}.{count}"
+    
+    headers.append(new_header)
+    data_dict[new_header] = data
 
-    # Convert to DataFrame
-    df = pd.DataFrame.from_dict(data_dict, orient='index').transpose()
+  # Convert to DataFrame
+  df = pd.DataFrame.from_dict(data_dict, orient='index').transpose()
 
-    #Rename columns to remove the prefix
-    new_columns = {col: col.replace(prefix, '') for col in df.columns}
-    df.rename(columns=new_columns, inplace=True)
+  #Rename columns to remove the prefix
+  new_columns = {col: col.replace(prefix, '') for col in df.columns}
+  df.rename(columns=new_columns, inplace=True)
 
-    # remove string artifacts (leading and ending "")
-    df = df.applymap(lambda x: x.strip('"') if isinstance(x, str) else x)
+  # remove string artifacts (leading and ending "")
+  df = df.applymap(lambda x: x.strip('"') if isinstance(x, str) else x)
 
-    return df
+  return df
