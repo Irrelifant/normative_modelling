257d2ec7a55c7ad38f45ab1411194a023d522a7d
Zeev-ops <116187472+Zeev-ops@users.noreply.github.com>
2024-08-24 15:18:47 +0000

    add install option for project

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Setup and install requirements..."
-
-pip install -r requirements.txt
-nbstripout --install --attributes .gitattributes --extra-keys 'kernelspec,metadata.language_info,widgets'
-
-echo "Setup finished."
