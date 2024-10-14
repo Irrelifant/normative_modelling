b27edf30c985f7c13ae58fc8e160b50fa1be24f5
Elias Schreiner <elias.schreiner@uni-tuebingen.de>
2024-10-14 10:12:52 +0000

    for PPMI set took the latestest column version of the counts

--- utils/helper.py
+++ utils/helper.py
@@ -64,6 +64,30 @@ def convert_gene_names_to_ensembl_mygene(gene_names):
   
   return ensembl_ids
 
+
+# Function to clean the 'age' column
+def clean_age(value):
+    import random
+    if isinstance(value, str) and 'age: ' in value:
+        if 'age: ' in value:
+            replaced = value.replace('age: ', '')
+            if replaced == 'NA':
+                return np.nan
+            return float(replaced)
+        else:
+            print(f'Error: {value}')
+            return np.nan
+    # here the over 90 handle need to be set
+    elif isinstance(value, str) and 'x' in value:
+        print(f'Error: {value}')
+
+        return np.nan
+    elif isinstance(value, str) and '90+' in value:
+        return float(random.randint(90, 99))
+    
+    return value
+
+
 def reorder_columns_by_metadata_and_gene_counts(df, metadata_cols=METADATA_COLS, gene_prefix='ENSG', gene_columns=[]):
     if len(gene_columns) == 0:
         gene_columns = [col for col in df.columns if col.startswith(gene_prefix)]  
@@ -132,3 +156,49 @@ def scatter_plot(df, x_ID='DUSP22', y_ID='age', hue_ID='study_accession'):
   plt.ylabel(f'{y_ID}')
   # plt.title(f'{x_ID} - {y_ID}')
   plt.show()
+def assemble_single_featureCounts_outputs_to_df(folder_path, file_endings='*.txt'):
+    file_list = glob.glob(os.path.join(folder_path, file_endings))
+
+    if len(file_list) == 0:
+        print('Error: Empty file list')
+        return
+    
+    dfs = []
+    for file in file_list:
+        df = pd.read_csv(file, skiprows=[0], sep='\t') # row 0 is a commented out metadata
+        df = df.iloc[:, [0, -1]]
+        sample_name = file.split('/')[-1] # remove the path infront of the filename
+        sample_name = '.'.join(sample_name.split('.')[1:-3]) # remove PPMI-Phase1-IR3 as thid inof is in filename later, and remove '.featureCounts.GencodeV29.txt' as this is redundant too
+        df.columns = ['Geneid', sample_name]
+        dfs.append(df)
+    combined_df = pd.concat(dfs, axis=1)
+    combined_df = combined_df.loc[:, ~combined_df.columns.duplicated()]
+    return df
+
+def keep_latest_ensamble_version(df, duplicates, start_col=11):
+    cols = list(df.iloc[:,start_col:].columns)
+    ensemble_names = [col.split('.')[0] for col in cols if '.' in col ]
+    ensemble_versions = [col.split('.')[1] for col in cols if '.' in col ]
+    remove_cols = []
+    for dupl in duplicates:
+        indexes = [index for index, value in enumerate(ensemble_names) if value == dupl]  
+        highest = 0 
+
+        for i in indexes:
+            ## There are a couple of _PAR_Y columns in PPMI, and i could not figure out what that is (Pseudoautosomal Region on Y Chromosome)
+            if '_' in ensemble_versions[i]:
+                ensemble_versions[i] = 0
+            if int(ensemble_versions[i]) > highest:
+                highest = int(ensemble_versions[i])
+        
+        while highest > 0:
+            remove_cols.append(dupl + '.' + str(highest))
+            highest = highest - 1
+    
+    print(df.shape)
+    df = df.drop(columns=remove_cols, errors='ignore') #As not all versions inbetween must exist
+    print(df.shape)
+
+    return df 
+
+
