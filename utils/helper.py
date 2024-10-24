9e824a9e57859dfdfbd4926eec5da76c2a391acf
Elias Schreiner <elias.schreiner@uni-tuebingen.de>
2024-10-24 10:23:37 +0000

    rebase origin master

--- utils/helper.py
+++ utils/helper.py
@@ -1,8 +1,7 @@
+import numpy as np
+import pandas as pd
 import os
-import seaborn as sns
-import mygene
-import matplotlib.pyplot as plt
-
+import glob
 
 METADATA_COLS = [
     'study_accession',
@@ -54,15 +53,17 @@ def convert_gene_names_to_ensembl(gene_names):
     return ensembl_ids
 
 def convert_gene_names_to_ensembl_mygene(gene_names):
-  mg = mygene.MyGeneInfo()
-  result = mg.querymany(gene_names, scopes='symbol', fields='ensembl.gene', species='human')
-  
-  ensembl_ids = []
-  for item in result:
-    ensembl_id = item.get('ensembl', {}).get('gene') if 'ensembl' in item else None
-    ensembl_ids.append(ensembl_id)
-  
-  return ensembl_ids
+    import mygene
+
+    mg = mygene.MyGeneInfo()
+    result = mg.querymany(gene_names, scopes='symbol', fields='ensembl.gene', species='human')
+    
+    ensembl_ids = []
+    for item in result:
+        ensembl_id = item.get('ensembl', {}).get('gene') if 'ensembl' in item else None
+        ensembl_ids.append(ensembl_id)
+    
+    return ensembl_ids
 
 
 # Function to clean the 'age' column
@@ -104,58 +105,10 @@ def reorder_columns_by_metadata_and_gene_counts(df, metadata_cols=METADATA_COLS,
     return reordered_df
 
 def get_negative_values(df):
-  description = df.describe().T
-  negative_mins = description[description['min'] < 0]
-  return negative_mins
+    description = df.describe().T
+    negative_mins = description[description['min'] < 0]
+    return negative_mins
 
-def violinplot_overall(df, x_topic='GENDER', y_topic='MAX_SUBJECT_AGE_IN_YEARS', hue_split='GENDER', bin_name='test_bin'):
-  sns.violinplot(x=x_topic, y=y_topic, hue=hue_split, data=df[[x_topic, y_topic]], split=True)
-  try:
-    os.makedirs(f'{bin_name}')
-  except FileExistsError:
-  # directory already exists
-    pass
-
-  if bin_name != 'test_bin':
-    plt.show()
-  else:
-    plt.savefig(f'{bin_name}/violin_overall.png')
-    
-    
-def manhattanplot(df, start_col=8, end_col=-1, save_name='default'):
-  if end_col == -1: 
-    markers = df.columns[start_col:]
-  else:
-    markers = df.columns[start_col:end_col]
-  expression_values = [df[col].median() for col in markers] 
-
-  try:
-    os.makedirs(f'{save_name}')
-  except FileExistsError:
-  # directory already exists
-    pass
-
-  # Plot
-  plt.figure(figsize=(10, 6))
-  plt.scatter(markers, expression_values, s=10, color='blue', alpha=0.5)
-  plt.xticks([])  # Remove x ticks
-  plt.xlabel('Marker Name')
-  plt.ylabel('Expression Value median')
-  plt.title('median Gene Expression Dot Plot')
-  plt.tight_layout()
-  plt.savefig(f'{save_name}/manhattan_plot.png')
-  plt.show()
-
-def scatter_plot(df, x_ID='DUSP22', y_ID='age', hue_ID='study_accession'):
-  plot = sns.jointplot(x=x_ID, y=y_ID, data=df, kind='scatter', hue=hue_ID)
-  
-  plot.ax_joint.legend_.remove()
-
-  #plot.ax_joint.scatter(df[x_ID], df[y_ID], c=df['study_accession'])
-  plt.xlabel(f'{x_ID}')
-  plt.ylabel(f'{y_ID}')
-  # plt.title(f'{x_ID} - {y_ID}')
-  plt.show()
 def assemble_single_featureCounts_outputs_to_df(folder_path, file_endings='*.txt'):
     file_list = glob.glob(os.path.join(folder_path, file_endings))
 
